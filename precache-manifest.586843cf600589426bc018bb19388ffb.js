b3a8112548bd18e99e8394301aa6977824f98c5e
Luiz F Zarco <felipezarco@hotmail.com>
2019-08-04 19:22:55 -0300

    Updates

---
+++ precache-manifest.586843cf600589426bc018bb19388ffb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f1833ca34a2a3349513496edba7df2",
+    "url": "/senheiro/index.html"
+  },
+  {
+    "revision": "9a3d50638dbcba489898",
+    "url": "/senheiro/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0e6354e7057439c209ef",
+    "url": "/senheiro/static/css/main.d1b6b6b4.chunk.css"
+  },
+  {
+    "revision": "9a3d50638dbcba489898",
+    "url": "/senheiro/static/js/2.6cc7f9a7.chunk.js"
+  },
+  {
+    "revision": "0e6354e7057439c209ef",
+    "url": "/senheiro/static/js/main.628d3398.chunk.js"
+  },
+  {
+    "revision": "4c88347a0a888b2d7264",
+    "url": "/senheiro/static/js/runtime~main.284307b4.js"
+  }
+]);
